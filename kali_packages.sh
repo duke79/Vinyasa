39b464db7e46b92c100079010cf77f01a23bbb39
Pulkit Singh Baliyan <pulkitsingh01@gmail.com>
2021-02-05 06:14:08 +0000

    Create kali_packages.sh

---
+++ kali_packages.sh
@@ -0,0 +1,15 @@
+#VSCode
+# Download from : https://code.visualstudio.com/download
+sudo apt install ./Downloads/code_*.deb
+
+#Stremio
+# Download from: https://www.stremio.com/downloads
+cd Downloads
+wget http://mirrors.kernel.org/ubuntu/pool/multiverse/f/fdk-aac/libfdk-aac1_0.1.6-1_amd64.deb
+sudo apt install ./libfdk-aac1_*_amd64.deb
+sudo apt install ./stremio*amd64.deb
+wget https://github.com/Stremio/stremio-bugs/issues/243#issuecomment-754149141
+sudo apt install libx264*_amd64.deb
+
+#Albert
+sudo apt install albert
